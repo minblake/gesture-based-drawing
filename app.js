88ecca449b7edd22e68007559d40522b0a043d1f
Min Gyu Park <parkmg@myumanitoba.ca>
2019-03-05 16:16:35 -0600

    Create canvas layout

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ new Vue({
     y: { curr: 0, next: 0 },
     listX: [],
     listY: [],
-    rect: [0, 0, 0, 0],
+    shapeSelected: [false, false, false, false], // [ line, triangle, rectangle, circle ]
     isMouseDown: false
   },
   mounted() {
-    this.canvas = document.querySelector("#canvas");
+    this.canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
     this.ctx.lineWidth = 2;
   },
@@ -55,7 +55,7 @@ new Vue({
       const height = maxY - minY;
 
       this.clearCanvas(minX, minY, width, height);
-      this.ctx.rect(minX, minY, width, height);
+      this.ctx.fillRect(minX, minY, width, height);
       this.ctx.stroke();
     }
   }
