03bb1b0f292ae289f17e505600c7f8101e54bfbd
Min Gyu Park <parkmg@myumanitoba.ca>
2019-03-06 14:38:15 -0600

    Add drawing on touch event

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
+Vue.directive("pan", {
+  bind: function(el, binding) {
+    if (typeof binding.value === "function") {
+      const mc = new Hammer(el);
+      mc.get("pan").set({ direction: Hammer.DIRECTION_ALL });
+      mc.on("pan", binding.value);
+    }
+  }
+});
+
 const vm = new Vue({
   el: "#app",
   data: {
     canvas: null,
+    canvasDim: { x: 0, y: 0 },
     ctx: null,
-    x: { curr: 0, next: 0 },
-    y: { curr: 0, next: 0 },
+    next: { x: 0, y: 0 },
+    curr: { x: 0, y: 0 },
     listX: [],
     listY: [],
     state: [],
-    shapeSelected: [true, false, false, false], // [ line, triangle, rectangle, circle ]
-    isMouseDown: false
+    shapeSelected: [true, false, false, false] // [ line, triangle, rectangle, circle ]
+    // isTouching: false
   },
   mounted() {
     this.canvas = document.getElementById("drawing-board");
     this.ctx = this.canvas.getContext("2d");
     this.ctx.lineWidth = 2;
+
+    const rect = this.canvas.getBoundingClientRect();
+    this.canvasDim.x = rect.left;
+    this.canvasDim.y = rect.top;
   },
   methods: {
+    onPan({ center, eventType }) {
+      eventType === 2
+        ? this.draw({
+            x: center.x - this.canvasDim.x,
+            y: center.y - this.canvasDim.y
+          })
+        : this.stopDrawing();
+    },
     stopDrawing() {
-      this.isMouseDown = false;
-      this.drawRectangle();
+      console.log("stop");
+      // this.isTouching = false;
+      // this.drawRectangle();
     },
-    startDrawing(e) {
-      this.isMouseDown = true;
-      this.x.curr = e.offsetX;
-      this.y.curr = e.offsetY;
-    },
-    draw(e) {
-      if (this.isMouseDown) {
-        this.x.next = e.offsetX;
-        this.y.next = e.offsetY;
+    draw(pos) {
+      this.next = { ...pos };
 
+      if (this.listX.length > 1) {
         this.ctx.beginPath();
-        this.ctx.moveTo(this.x.curr, this.y.curr);
-        this.ctx.lineTo(this.x.next, this.y.next);
+        this.ctx.moveTo(this.curr.x, this.curr.y);
+        this.ctx.lineTo(this.next.x, this.next.y);
         this.ctx.stroke();
-
-        this.x.curr = this.x.next;
-        this.y.curr = this.y.next;
-
-        this.listX.push(this.x.curr, this.x.next);
-        this.listY.push(this.y.curr, this.y.next);
       }
+      this.curr = { ...this.next };
+
+      this.listX.push(this.next.x);
+      this.listY.push(this.next.y);
     },
     clearCanvas() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -56,6 +72,7 @@ const vm = new Vue({
       const width = maxX - minX;
       const height = maxY - minY;
 
+      console.log(this.listX);
       this.clearCanvas();
       this.ctx.fillRect(minX, minY, width, height);
       this.ctx.stroke();
